ac910fef0771e152316680f04763593cb7f75fc2
OhJia <jiajia0919@gmail.com>
2015-06-30 11:33:09 -0400

    added onDeviceMove example

---
+++ onDeviceMove/sketch.js
@@ -0,0 +1,17 @@
+var value = 0;
+
+function setup() {
+    var myCan = createCanvas(windowWidth, windowHeight);
+    myCan.parent('p5Container');
+}
+
+function draw() {
+  background(value);
+}
+function onDeviceMove() {
+	value = value + 5;
+	if (value > 255) {
+	value = 0;
+	}
+}
+
